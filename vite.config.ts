653a71fb1cd0aee45d64cd1d51699e3e36544c47
Yuuji Kamura <your-email@example.com>
2026-01-23 11:48:41 +0900

    Add sourcemap for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -53,6 +53,7 @@ export default defineConfig(({ mode }) => {
         }
       },
       build: {
+        sourcemap: true,
         rollupOptions: {
           output: {
             manualChunks: (id) => {
